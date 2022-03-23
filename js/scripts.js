2303ec54f0f8d888ef0d791f1bb18f35372ca94f
Jinan V <j.vyent@gmail.com>
2022-03-23 18:04:32 +0100

    Added loops to scripts.js
    added a 'for' loop and an 'if' loop. Need to still change how the list is set up, because the 'if' loop is not displaying properly now.

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,21 @@ let pokemonList = [
     weight: 60,
     type: ['ground', 'poison']
   }
-]
+];
 
-let pokemonList = ["Jigglypuff", "Butterfree", "Nidoqueen"];
-for (let i = 0; i < pokemonList.length; i++){
-  console.log(pokemonList[i])  
+//this is to display the name of the app; it still needs proper styling
+document.write('<h1>Pokemon Encyclopedia</h1>');
+
+//this is to create an unordered list of the items in pokemonList
+document.write('<ul class="ul">')
+
+//this is the for loop I created to display the name and height of the Pokemon on a loop)
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<li class="listitem"> ' + pokemonList[i].name + ' ' + 'Height: ' + pokemonList[i].height + '</li>');
 }
+
+if (pokemonList => 35){
+  document.write(' - The big boss is here!')
 }
+
+document.write('</ul>')
