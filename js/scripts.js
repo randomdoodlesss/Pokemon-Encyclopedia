afca63262356086f547fb9f3d4ad0007cb69377a
Jinan V <j.vyent@gmail.com>
2022-04-01 17:15:52 +0200

    Updated scripts.js
    Changed my coding to make it look more clear and compact. also changed to paragraph (p) instead of an unordered list (ul) for clarity and cleanliness of the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,14 @@
 let pokemonList = [
-  {
-    name: 'Jigglypuff',
-    height: 0.5,
-    weight: 5.5,
-    type: ['fairy', 'normal']
-  },
-
-  {
-    name: 'Butterfree',
-    height: 1.1,
-    weight: 32,
-    type: ['bug', 'flying']
-  },
-
-  {
-    name: 'Nidoqueen',
-    height: 1.3,
-    weight: 60,
-    type: ['ground', 'poison']
-  }
+  { name: 'Jigglypuff', height: 0.5, weight: 5.5, type: ['fairy', 'normal'] },
+  {name: 'Butterfree', height: 1.1, weight: 32, type: ['bug', 'flying'] },
+  {name: 'Nidoqueen', height: 1.3, weight: 60, type: ['ground', 'poison'] },
 ];
-
-//this is to display the name of the app; it still needs proper styling
-document.write('<h1>Pokemon Encyclopedia</h1>');
-
-//this is to create an unordered list of the items in pokemonList
-document.write('<ul class="ul">')
 
 //this is the for loop I created to display the name and weight of the Pokemon on a loop, it includes a conditional for the weight value)
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].weight >= 35){
-    document.write('<li class="listitem"> ' + pokemonList[i].name + ' ' + 'weight: ' + pokemonList[i].weight + ' - The big boss is here' + '</li>');
+    document.write("<p>" + pokemonList[i].name + ' ' + 'weight: ' + pokemonList[i].weight + ' - The big boss is here' + "</p>");
   }
   else
-    document.write('<li class="listitem"> ' + pokemonList[i].name + ' ' + 'weight: ' + pokemonList[i].weight + '</li>');
+  document.write("<p>" + pokemonList[i].name + ' ' + 'weight: ' + pokemonList[i].weight + "</p>");
 }
-
-document.write('</ul>')
